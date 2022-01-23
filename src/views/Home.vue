fd2eb79973775eee1d8ae202e3ec3af1f631b377
Hafiz <mdnur.hafiz@outlook.com>
2022-01-24 01:06:40 +0800

    Set up boilerplate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from "vue";
+import { defineComponent, reactive, ref, toRefs } from "vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    const state = reactive({});
+
+    return {
+      ...toRefs(state),
+    };
   },
 });
 </script>
