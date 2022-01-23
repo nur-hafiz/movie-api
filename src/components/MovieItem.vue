81c5f6b0b550729fe1fdcf39d764982273e0e376
Hafiz <mdnur.hafiz@outlook.com>
2022-01-24 03:35:29 +0800

    Create MovieItem component

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ movie.Title }}
+    Release Year: {{ movie.Year }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Movie from "@/types/Movie";
+
+export default defineComponent({
+  props: {
+    movie: {
+      required: true,
+      type: Object as () => Movie,
+    },
+  },
+});
+</script>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -2,6 +2,7 @@
   <div>
     <ul>
       <li v-for="movie in movies" :key="movie.imdbID">
+        <MovieItem :movie="movie" />
       </li>
     </ul>
   </div>
@@ -10,8 +11,10 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import Movie from "@/types/Movie";
+import MovieItem from "@/components/MovieItem.vue";
 
 export default defineComponent({
+  components: { MovieItem },
 
   props: {
     movies: {
