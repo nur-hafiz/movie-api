bf5c7dde4bc96bb2d2768abcf19f8996f5469fa1
Hafiz <mdnur.hafiz@outlook.com>
2022-01-24 20:32:00 +0800

    Create Favourite class as a store

---
+++ src/store/Favourites.ts
@@ -0,0 +1,23 @@
+import Movie from "@/types/Movie";
+
+class Favourites {
+  private list: Movie[];
+
+  constructor() {
+    this.list = [];
+  }
+
+  public hasMovie(movie: Movie): boolean {
+    return this.list.some((item) => item.imdbID === movie.imdbID);
+  }
+
+  public addMovie(movie: Movie): void {
+    this.list.push(movie);
+  }
+
+  public removeMovie(movie: Movie): void {
+    this.list = this.list.filter((item) => item.imdbID !== movie.imdbID);
+  }
+}
+
+export default Favourites;
