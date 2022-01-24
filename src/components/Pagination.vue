80215036f6d3caf1cebb2e2c8fbc53248d638210
Hafiz <mdnur.hafiz@outlook.com>
2022-01-24 19:11:18 +0800

    Boilerplate for pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <li>First</li>
+        <li>Previous</li>
+        <li>Current</li>
+        <li>Next</li>
+        <li>Last</li>
+    </ul>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+      page: {
+          required: true,
+          type: Number
+      }
+  },
+  setup() {
+  },
+
+  methods: {
+    jumpToFirst() {
+    },
+
+    goToPage(page: number) {
+    },
+
+    jumpToLast() {
+    }
+  },
+
+  mounted() {
+  },
+});
+</script>
